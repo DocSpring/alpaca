3f8266efdb13a68e3a265f237e40820098aabeb8
Nathan Broadbent <git@ndbroadbent.com>
2019-05-23 16:28:19 +0700

    Renamed repo

--- build-and-copy.sh
+++ build-and-copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 gulp default
-cp build/alpaca/bootstrap/alpaca.js ../form_api/vendor/assets/javascripts/alpaca.js
-cp build/alpaca/bootstrap/alpaca.css ../form_api/vendor/assets/stylesheets/alpaca.css
+cp build/alpaca/bootstrap/alpaca.js ../docspring/vendor/assets/javascripts/alpaca.js
+cp build/alpaca/bootstrap/alpaca.css ../docspring/vendor/assets/stylesheets/alpaca.css
